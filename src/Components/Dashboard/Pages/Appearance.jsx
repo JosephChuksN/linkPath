1fbc1dcdcb11e0f69779ba430a18b8c8b54b06bc
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-09-27 00:44:06 +0800

    Fixed issue with appearance section

--- src/Components/Dashboard/Pages/Appearance.jsx
+++ src/Components/Dashboard/Pages/Appearance.jsx
@@ -24,38 +24,38 @@ const handleImgUpload = (e)=>{
 
   return (
     <div className='flex flex-col lg:flex-row mx-2 gap-3'>
-        <div className='w-full lg:w-[55%]'>
+        <div className='w-full lg:w-[55%] flex flex-col gap-1'>
             <span className='text-cyan-700 font-semibold '>LAYOUT</span>
-         <div className='w-full items-center mx-auto  border p-5 rounded bg-gray-100'>
+         <div className='w-full items-center mx-auto  border p-5 rounded '>
             <span className='font-medium '>Choose a layout of your Linkpath</span>
               
-        <div className='flex md:flex-row  flex-col-reverse w-full items-center mx-auto gap-5 mt-3'>
-            <span className='absolute w-8 h-8 rounded-full bg-green-500 lg:left-[19rem] md:left-[24rem] md:top-[15.5rem] right-4 top-[17rem] flex items-center justify-center'> <FontAwesomeIcon className='text-white' icon={faCheck} /></span>
-    <div className=' w-full rounded bg-white'>
-    <div className='flex flex-col items-center justify-center gap-3'>
-       <div className='flex flex-col w-full items-center justify-center gap-2 pt-3'>
-       <span className='w-24 h-24 bg-cyan-600/10 rounded-full'></span>
-       <span className='h-8 w-2/5 bg-cyan-600/10 rounded-md'></span>
+        <div className='flex md:flex-row-reverse  flex-col-reverse w-2/3  lg:px-0 px-3 items-center mx-auto gap-5 mt-3'>
+            <span className='absolute w-5 h-5 rounded-full bg-green-500 lg:left-[19.3rem] md:left-[24rem] md:top-[15.3rem] right-[5.5rem] top-[17.5rem] flex items-center justify-center'> <FontAwesomeIcon className='text-white text-sm' icon={faCheck} /></span>
+    <div className=' w-full rounded-lg py-2 bg-cyan-600/10'>
+    <div className='flex flex-col items-center justify-center gap-2'>
+       <div className='flex flex-col w-full items-center justify-center gap-1 pt-3'>
+       <span className='w-16 h-16 bg-slate-900/30 rounded-full'></span>
+       <span className='h-6 w-2/5  bg-cyan-600 rounded-md'></span>
        </div>
-       <div className='flex flex-col w-full items-center gap-3 my-2'>
-        <span className='w-3/4 h-16 bg-cyan-600/10 rounded-md'></span>
-        <span className='w-3/4 h-16 bg-cyan-600/10 rounded-md'></span>
-        <span className='w-3/4 h-16 bg-cyan-600/10 rounded-md'></span>
-        <span className='w-3/4 h-16 bg-cyan-600/10 rounded-md'></span>
+       <div className='flex flex-col w-full items-center gap-2 my-2'>
+        <span className='w-3/4 h-8 bg-white rounded-md'></span>
+        <span className='w-3/4 h-8 bg-white rounded-md'></span>
+        <span className='w-3/4 h-8 bg-white rounded-md'></span>
+        <span className='w-3/4 h-8 bg-white rounded-md'></span>
        </div>
     </div>
     </div>
-    <div className='w-full py-6 rounded bg-white'>
+    <div className='w-full py-6 rounded-lg bg-cyan-600/10'>
     <div className='flex flex-col items-center justify-center  '>
        <div className='flex flex-col w-full items-center justify-center'>
-       <span className='w-3/4 h-36 border flex items-center justify-center'><span className='w-[70%] h-[85%] border bg-slate-900/10'></span></span>
-       <span className='h-16 w-3/4 bg-slate-900/10 border-b'></span>
+       <span className='w-3/4 h-24 border flex items-center justify-center bg-slate-900/10'><span className='w-[70%] h-[85%] border bg-slate-900/30'></span></span>
+       <span className='h-7 w-3/4 bg-cyan-600 border-b'></span>
        </div>
        <div className='flex flex-col w-full items-center'>
-        <span className='w-3/4 h-14  bg-cyan-600/10 border-b '></span>
-        <span className='w-3/4 h-14 bg-cyan-600/10 border-b '></span>
-        <span className='w-3/4 h-14 bg-cyan-600/10 border-b '></span>
-        <span className='w-3/4 h-14 bg-cyan-600/10 border-b'></span>
+        <span className='w-3/4 h-8 bg-white border-b '></span>
+        <span className='w-3/4 h-8 bg-white border-b '></span>
+        <span className='w-3/4 h-8 bg-white border-b '></span>
+        <span className='w-3/4 h-8 bg-white border-b'></span>
        </div>
     </div>
     </div>
