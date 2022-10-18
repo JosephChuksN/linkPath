ca552168dd006053512d19df8105ff63a3976306
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-10-18 22:21:46 +0800

    fixing api call origin issue

--- src/Context/AppContext.js
+++ src/Context/AppContext.js
@@ -91,7 +91,7 @@ const login = async (email, password) =>{
   try {
     setError('')
     setLoading(true)
-  const {data} =  await axios.post('https://infinite-earth-50565.herokuapp.com/api/v1/auth/login', { email, password}, {headers:{Origin: 'https://linkpath-josephn.vercel.app'}})
+  const {data} =  await axios.post('https://infinite-earth-50565.herokuapp.com/api/v1/auth/login', { email, password}, {headers:{'Origin': 'https://linkpath-josephn.vercel.app'}})
     
     const {user, token} = data
     addToLocalStorage({user, token})
