ef047ad5d9c0540277a7fc0aa20a20f8fd3e4666
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2023-07-25 08:38:20 +0800

    fixed landing page issue

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -46,9 +46,9 @@ const NavBar = () => {
       <a href="/"><span className=' text-2xl flex items-center text-cyan-600 font-medium'>LINKPATH</span></a>
       <span onClick={handleShowNav} className="lg:hidden text-2xl"><FontAwesomeIcon icon={faBars} /></span>
    </div>
-   <div ref={sidebarRef} className={`flex lg:h-auto h-[100vh] lg:bg-transparent bg-white lg:flex-row flex-col lg:justify-between items-center  lg:gap-28 lg:static  absolute lg:w-3/5 w-[70%] border-r-[1px] top-0  transition-all duration-700 delay-100 ease-in-out  ${showNav ? "left-0" : "-left-96"}`}>
+   <div ref={sidebarRef} className={`flex lg:h-auto h-[100vh] lg:bg-transparent bg-white lg:flex-row flex-col lg:justify-between items-center  lg:gap-28 lg:static  absolute lg:w-3/5 w-[70%] lg:border-0 border-r-[1px] top-0  transition-all duration-700 delay-100 ease-in-out  ${showNav ? "left-0" : "-left-96"}`}>
    <span  onClick={handleShowNav} className={` lg:hidden text-3xl absolute right-3 top-2 text-cyan-600  px-5 py-3`}><FontAwesomeIcon icon={faXmark}  /></span>
-   <div className='flex lg:flex-row flex-col gap-3 mb-10 mt-28 lg:my-0 justify-start w-32 lg:w-auto'>
+   <div className='flex lg:flex-row flex-col gap-3 mb-10 mt-28 lg:my-0 lg:justify-center justify-start w-32 lg:w-auto'>
      <a onClick={handleShowNav} href="#guide"><span  className='flex  items-center font-medium lg:px-2 py-1 text-lg cursor-pointer transition-all delay-75 duration-300 ease-in-out hover:text-cyan-600 hover:underline underline-offset-4 '>How It Works</span></a>
      <a onClick={handleShowNav} href="#contact"><span  className='flex  items-center font-medium lg:px-2 py-1  text-lg cursor-pointer transition-all delay-75 duration-300 ease-in-out hover:text-cyan-600 hover:underline underline-offset-4   '>Contact</span></a>
    </div>
@@ -59,7 +59,7 @@ const NavBar = () => {
     </span>
    </Link>
    <Link to={user? "dashboard" : "signup"} onClick={handleShowNav}  >
-    <span className='flex justify-center items-center text-cyan-600  w-2/3 lg:w-autopx-2 py-1 shadow-md shadow-gray-300 border border-cyan-600 rounded-lg text-sm hover:border-cyan-300 hover:shadow-cyan-300 hover:bg-cyan-300 hover:text-white hover:scale-105  transition-all delay-75 duration-300 ease-in-out  ' >
+    <span className='flex justify-center items-center text-cyan-600  w-2/3 lg:w-auto px-2 py-1 shadow-md shadow-gray-300 border border-cyan-600 rounded-lg text-sm hover:border-cyan-300 hover:shadow-cyan-300 hover:bg-cyan-300 hover:text-white hover:scale-105  transition-all delay-75 duration-300 ease-in-out  ' >
       SIGN UP
     </span>
    </Link>
