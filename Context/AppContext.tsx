a6f1ce8f3cbecf5a577f852028ea8d0a5fb4036b
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2023-09-09 18:26:59 +0800

    fix

--- Context/AppContext.tsx
+++ Context/AppContext.tsx
@@ -44,10 +44,10 @@ export const AuthProvider = ({children}:Props) =>{
 
   
   const [links, setLinks] = useState<Links[] | null>(null)
-  const currentUser: null | string = window?.localStorage?.getItem("user");
+  const currentUser: null | string = typeof window !== 'undefined' ? localStorage?.getItem("user") : null;
   const user: User | null = currentUser ? JSON.parse(currentUser) : null;
-  const token: null | string = window?.localStorage?.getItem("token");
-  const description: null | string = window?.localStorage?.getItem("bio");
+  const token: null | string = typeof window !== 'undefined' ? localStorage?.getItem("token") : null;
+  const description: null | string = typeof window !== 'undefined' ? localStorage?.getItem("bio") : null;
   const [loading, setLoading] = useState<boolean>(false)
   const [regError, setRegError] = useState<string>('')
   const [loginError, setLoginError] = useState<string>('')
@@ -56,13 +56,7 @@ export const AuthProvider = ({children}:Props) =>{
   const [emailVerifiedLogin, setEmailVerifiedLogin] = useState<string>("");
   const { push }= useRouter();
   
-// useEffect(()=>{
-//   if(typeof window !== undefined){
-//      setCurrentUser(localStorage?.getItem("user"));
-//      setToken(localStorage?.getItem("token"));
-//      setDescription(localStorage?.getItem("bio"));
-//   }
-// },[currentUser, token])
+
 
   const authFetch = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ReactNode } from 'react'
-import { redirect } from 'next/navigation';
+import {  useRouter } from 'next/navigation';
 import DesktopView from '@components/Dashboard/DesktopView';
 import Header from '@components/Dashboard/Header';
 import DashboardNav from '@components/Dashboard/DashboardNav';
@@ -12,19 +12,24 @@ export default function DashboardLayout({
 } : {children: ReactNode}
 ){
    const { token } = useAuth()
+   const { push } = useRouter()
 
-   if(!token){
-    redirect('/login')
-   }
+   
 
     return (
-      <main className="lg:flex lg:mx-6  bg-white">
-        <div className="flex flex-col  h-full min-h-screen w-full border-x xl:w-3/4 relative">
-          <Header />  
-          <DashboardNav />
-          {children}
-        </div>
-        <DesktopView />
-      </main>
+      <>
+        {token ? (
+          <main className="lg:flex lg:mx-6  bg-white">
+            <div className="flex flex-col  h-full min-h-screen w-full border-x xl:w-3/4 relative">
+              <Header />
+              <DashboardNav />
+              {children}
+            </div>
+            <DesktopView />
+          </main>
+        ) : (
+          push("/login")
+        )}
+      </>
     );
 }
