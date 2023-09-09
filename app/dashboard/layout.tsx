d5c6384382066daf338b3b9009ec6cb32976fdf0
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2023-09-09 18:32:15 +0800

    fix

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ReactNode } from 'react'
-import {  useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import DesktopView from '@components/Dashboard/DesktopView';
 import Header from '@components/Dashboard/Header';
 import DashboardNav from '@components/Dashboard/DashboardNav';
@@ -14,22 +14,18 @@ export default function DashboardLayout({
    const { token } = useAuth()
    const { push } = useRouter()
 
-   
+   if(!token){
+    push('/login')
+   }
 
     return (
-      <>
-        {token ? (
-          <main className="lg:flex lg:mx-6  bg-white">
-            <div className="flex flex-col  h-full min-h-screen w-full border-x xl:w-3/4 relative">
-              <Header />
-              <DashboardNav />
-              {children}
-            </div>
-            <DesktopView />
-          </main>
-        ) : (
-          push("/login")
-        )}
-      </>
+      <main className="lg:flex lg:mx-6  bg-white">
+        <div className="flex flex-col  h-full min-h-screen w-full border-x xl:w-3/4 relative">
+          <Header />  
+          <DashboardNav />
+          {children}
+        </div>
+        <DesktopView />
+      </main>
     );
 }
